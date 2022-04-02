ad7a385a7e86ee7d1bed643528909188ba281ad3
Strelya <serdjiostrel@gmail.com>
2022-04-03 02:09:06 +0300

    Sidebar widgets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('layouts.sidebar', function ($view) {
+            if (Cache::has('cats')) {
+                $cats = Cache::get('cats');
+            } else {
+                $cats = Category::withCount('posts')->orderByDesc('posts_count')->get();
+                Cache::put('cats', $cats, 30);
+            }
+
+            $view->with('popular_posts', Post::orderByDesc('views')->limit(3)->get());
+            $view->with('cats', $cats);
+        });
     }
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,29 +3,16 @@
                                 <h2 class="widget-title">Popular Posts</h2>
                                 <div class="blog-list-widget">
                                     <div class="list-group">
-                                        <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
+                                        @foreach ($popular_posts as $popular_post)
+                                        <a href="{{ route('posts.single', ['slug' => $popular_post->slug])}}" class="list-group-item list-group-item-action flex-column align-items-start">
                                             <div class="w-100 justify-content-between">
-                                                <img src="/assets/front/upload/small_07.jpg" alt="" class="img-fluid float-left">
-                                                <h5 class="mb-1">5 Beautiful buildings you need to before dying</h5>
-                                                <small>12 Jan, 2016</small>
+                                                <img src="{{ $popular_post->getImage()}}" alt="" class="img-fluid float-left">
+                                                <h5 class="mb-1">{{ $popular_post->title}}</h5>
+                                                <small>{{ $popular_post->getPostdate()}}</small>
+                                                <small>| <i class="fa fa-eye"></i> {{ $popular_post->views}}</small>
                                             </div>
                                         </a>
-
-                                        <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
-                                            <div class="w-100 justify-content-between">
-                                                <img src="/assets/front/upload/small_08.jpg" alt="" class="img-fluid float-left">
-                                                <h5 class="mb-1">Let's make an introduction for creative life</h5>
-                                                <small>11 Jan, 2016</small>
-                                            </div>
-                                        </a>
-
-                                        <a href="marketing-single.html" class="list-group-item list-group-item-action flex-column align-items-start">
-                                            <div class="w-100 last-item justify-content-between">
-                                                <img src="/assets/front/upload/small_09.jpg" alt="" class="img-fluid float-left">
-                                                <h5 class="mb-1">Did you see the most beautiful sea in the world?</h5>
-                                                <small>07 Jan, 2016</small>
-                                            </div>
-                                        </a>
+                                        @endforeach
                                     </div>
                                 </div><!-- end blog-list -->
                             </div><!-- end widget -->
@@ -34,13 +21,9 @@
                                 <h2 class="widget-title">Categories</h2>
                                 <div class="link-widget">
                                     <ul>
-                                        <li><a href="#">Marketing <span>(21)</span></a></li>
-                                        <li><a href="#">SEO Service <span>(15)</span></a></li>
-                                        <li><a href="#">Digital Agency <span>(31)</span></a></li>
-                                        <li><a href="#">Make Money <span>(22)</span></a></li>
-                                        <li><a href="#">Blogging <span>(66)</span></a></li>
-                                        <li><a href="#">Entertaintment <span>(11)</span></a></li>
-                                        <li><a href="#">Video Tuts <span>(87)</span></a></li>
+                                        @foreach ($cats as $cat)
+                                        <li><a href="{{ route('categories.single', ['slug' => $cat->slug]) }}">{{ $cat->title}} <span>({{ $cat->posts_count}})</span></a></li>
+                                        @endforeach
                                     </ul>
                                 </div><!-- end link-widget -->
                             </div><!-- end widget -->
